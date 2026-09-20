6479cda5b3f712187e88a96fd2129938da922012
SQ4KOU <sq4kou@wp.pl>
2026-09-20 20:58:22 +0200

    R10 Log4OM: add static postchecks

---
+++ jtdx-final/patch_log4om_postcheck_r10.py
@@ -0,0 +1,25 @@
+# --- postchecks ---------------------------------------------------------------
+checks = {
+    'CMakeLists.txt': [
+        'WebSockets Sql REQUIRED', 'Qt5::Sql', 'JTDX_QSQLITE_DLL',
+        'DESTINATION plugins/sqldrivers',
+    ],
+    'logbook/logbook.h': ['ADIF _workedLog;', 'loadLog4OM'],
+    'logbook/logbook.cpp': [
+        'QSQLITE_OPEN_READONLY', 'PRAGMA query_only = ON', 'SELECT * FROM \\"Log\\"',
+        '_workedLog.match', '_workedLog.add', 'return _log.getCount(mod);',
+        'Deliberately do not filter historical Log4OM QSOs by MY_GRIDSQUARE.',
+    ],
+    'Configuration.cpp': [
+        'Use Log4OM SQLite database for Worked/New', 'Log4OMEnabled', 'Log4OMDatabase',
+        'ui_->verticalLayout_9->insertWidget(0, log4om_group_)', ':/decpasses.png',
+    ],
+    'mainwindow.cpp': ['log4omWatcher', 'updateLog4OMWatcher', 'Log4OM Worked/New history reloaded'],
+}
+for path, needles in checks.items():
+    _, data = load(path)
+    for needle in needles:
+        if needle not in data:
+            raise SystemExit(f'[FAIL] R10 postcheck {path}: missing {needle!r}')
+
+print('[PASS] R10 Log4OM SQLite Worked/New integration applied; local ADIF counter preserved; read-only fallback enabled')
